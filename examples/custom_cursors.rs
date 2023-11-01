19ccf5b59edc9c2579fd817a4499e130c8e4917a
Eero Lehtinen <eero@eerolehtinen.fi>
2023-11-01 21:39:51 +0200

    Add baseline impl for web and x11

--- examples/custom_cursors.rs
+++ examples/custom_cursors.rs
@@ -1,88 +1,87 @@
-#![allow(clippy::single_match)]
+#![allow(clippy::single_match, clippy::disallowed_methods)]
 
+#[cfg(not(wasm_platform))]
 use simple_logger::SimpleLogger;
 use winit::{
     event::{ElementState, Event, KeyEvent, WindowEvent},
     event_loop::EventLoop,
-    window::{CursorIcon, WindowBuilder},
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
 };
 
 #[path = "util/fill.rs"]
 mod fill;
 
 fn main() -> Result<(), impl std::error::Error> {
-    SimpleLogger::new().init().unwrap();
+    #[cfg(not(wasm_platform))]
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
+    #[cfg(wasm_platform)]
+    console_log::init_with_level(log::Level::Debug).unwrap();
+
+    log::info!("asdf");
+
     let event_loop = EventLoop::new().unwrap();
+    let builder = WindowBuilder::new().with_title("A fantastic window!");
+    #[cfg(wasm_platform)]
+    let builder = {
+        use winit::platform::web::WindowBuilderExtWebSys;
+        builder.with_append(true)
+    };
+    let window = builder.build(&event_loop).unwrap();
 
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    window.set_title("A fantastic window!");
+    let mut cursor_key = 0;
+    let mut cursor_visible = true;
 
-    let mut cursor_idx = 0;
+    let cursor_image_bytes = include_bytes!("../cross.png").to_vec();
+    let cursor_image_bytes2 = include_bytes!("../cross2.png").to_vec();
 
-    event_loop.run(move |event, elwt| {
-        if let Event::WindowEvent { event, .. } = event {
-            match event {
-                WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            state: ElementState::Pressed,
-                            ..
-                        },
-                    ..
-                } => {
-                    println!("Setting cursor to \"{:?}\"", CURSORS[cursor_idx]);
-                    window.set_cursor_icon(CURSORS[cursor_idx]);
-                    if cursor_idx < CURSORS.len() - 1 {
-                        cursor_idx += 1;
-                    } else {
-                        cursor_idx = 0;
-                    }
+    window.register_custom_cursor_icon(0, cursor_image_bytes, 7, 7);
+    window.register_custom_cursor_icon(1, cursor_image_bytes2, 7, 7);
+
+    event_loop.run(move |event, _elwt| match event {
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(code),
+                        ..
+                    },
+                ..
+            } => match code {
+                KeyCode::KeyA => {
+                    log::debug!("Setting cursor to {:?}", cursor_key);
+                    window.set_custom_cursor_icon(cursor_key);
+                    cursor_key = (cursor_key + 1) % 2;
                 }
-                WindowEvent::RedrawRequested => {
-                    fill::fill_window(&window);
+                KeyCode::KeyS => {
+                    log::debug!("Setting cursor icon to default");
+                    window.set_cursor_icon(Default::default());
                 }
-                WindowEvent::CloseRequested => {
-                    elwt.exit();
+                KeyCode::KeyD => {
+                    cursor_visible = !cursor_visible;
+                    log::debug!("Setting cursor visibility to {:?}", cursor_visible);
+                    window.set_cursor_visible(cursor_visible);
                 }
-                _ => (),
+
+                _ => {}
+            },
+            WindowEvent::RedrawRequested => {
+                #[cfg(not(wasm_platform))]
+                fill::fill_window(&window);
             }
+            WindowEvent::CloseRequested => {
+                #[cfg(not(wasm_platform))]
+                _elwt.exit();
+            }
+            _ => (),
+        },
+        Event::AboutToWait => {
+            window.request_redraw();
         }
+        _ => {}
     })
 }
-
-const CURSORS: &[CursorIcon] = &[
-    CursorIcon::Default,
-    CursorIcon::Crosshair,
-    CursorIcon::Pointer,
-    CursorIcon::Move,
-    CursorIcon::Text,
-    CursorIcon::Wait,
-    CursorIcon::Help,
-    CursorIcon::Progress,
-    CursorIcon::NotAllowed,
-    CursorIcon::ContextMenu,
-    CursorIcon::Cell,
-    CursorIcon::VerticalText,
-    CursorIcon::Alias,
-    CursorIcon::Copy,
-    CursorIcon::NoDrop,
-    CursorIcon::Grab,
-    CursorIcon::Grabbing,
-    CursorIcon::AllScroll,
-    CursorIcon::ZoomIn,
-    CursorIcon::ZoomOut,
-    CursorIcon::EResize,
-    CursorIcon::NResize,
-    CursorIcon::NeResize,
-    CursorIcon::NwResize,
-    CursorIcon::SResize,
-    CursorIcon::SeResize,
-    CursorIcon::SwResize,
-    CursorIcon::WResize,
-    CursorIcon::EwResize,
-    CursorIcon::NsResize,
-    CursorIcon::NeswResize,
-    CursorIcon::NwseResize,
-    CursorIcon::ColResize,
-    CursorIcon::RowResize,
-];
